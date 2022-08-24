b50dbd4e8d8365cc35ef7cc4fe35137bae804f3b
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-08-24 14:39:56 -0300

    req-alterado-9

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,13 +4,6 @@ function SearchBar() {
   return (
     <div>
       <legend>Pesquisa</legend>
-      <label htmlFor="text">
-        <input
-          name="text"
-          type="text"
-          data-testid="search-input"
-        />
-      </label>
     </div>
   );
 }
