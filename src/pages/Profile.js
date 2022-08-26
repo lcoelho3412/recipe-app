34fc34dc37aaf885ad14a3a68751ef85a8e7e760
Daniel Campelo <danielcampelo159@gmail.com>
2022-08-26 15:17:13 -0300

    Realiza requisitos 57 ao 62 menos requisito 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,34 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+
+  function handleEmail() {
+    const email = JSON.parse(localStorage.getItem('user'));
+    return email.email;
+  }
+
+  function cleanLocalStorage() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div>
       <Header title="Profile" enalbleSearchIcon />
       <form>
-        <label htmlFor="input-email">
-          <input
-            data-testid="profile-email"
-            name="email"
-            placeholder="Digite seu email"
-          />
-        </label>
+        <p data-testid="profile-email">{handleEmail()}</p>
         <button
           type="button"
+          name="Done Recipes"
           data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
         >
           Done Recipes
         </button>
         <button
           type="button"
+          name="Favorite Recipes"
           data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
         >
           Favorites Recipes
         </button>
         <button
           type="button"
+          name="Logout"
           data-testid="profile-logout-btn"
+          onClick={ cleanLocalStorage }
         >
           Logout
         </button>
