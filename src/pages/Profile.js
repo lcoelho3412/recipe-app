f7ae353ff621618678645bc6553fd7e7f7b83bff
Daniel Campelo <danielcampelo159@gmail.com>
2022-08-26 12:59:23 -0300

    Realiza requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,33 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" enalbleSearchIcon />
+      <form>
+        <label htmlFor="input-email">
+          <input
+            data-testid="profile-email"
+            name="email"
+            placeholder="Digite seu email"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorites Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </form>
       <Footer />
     </div>
   );
